21db03d2f48f5da2346d2f30006f9b0ac37def3e
kamleshtayade <kamleshtayade@gmail.com>
2026-04-15 03:19:42 +0530

    Parallel programming

---
+++ notebooks/multiproc_helpers.py
@@ -0,0 +1,10 @@
+def print_numbers():
+    """Print numbers 1..5 (worker function)."""
+    for i in range(1, 6):
+        print(i)
+
+
+def print_letters():
+    """Print letters a..e (worker function)."""
+    for letter in 'abcde':
+        print(letter)
